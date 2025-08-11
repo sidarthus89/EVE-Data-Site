21c07a47754d23dc193bca76d213f442a15165bf
Sidarthus89 <sidarthus89@gmail.com>
2025-08-11 09:26:12 -0400

    Updates

---
+++ assets/Minerals-nXM7nraf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DrUdDapf.js";function n(){return e.jsxs("section",{className:"minerals",children:[e.jsx("h1",{children:"Minerals"}),e.jsx("p",{children:"In development"})]})}export{n as default};
