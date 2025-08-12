14ee36d18a752e14faaa129fe6ecb78a80567ef6
Sidarthus89 <sidarthus89@gmail.com>
2025-08-11 22:55:25 -0400

    Updates

---
+++ assets/Minerals-BQw9fkhG.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-8so4yUmv.js";function n(){return e.jsxs("section",{className:"minerals",children:[e.jsx("h1",{children:"Minerals"}),e.jsx("p",{children:"In development"})]})}export{n as default};
--- assets/Minerals-CXfbnMQP.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-UnInqtRS.js";function n(){return e.jsxs("section",{className:"minerals",children:[e.jsx("h1",{children:"Minerals"}),e.jsx("p",{children:"In development"})]})}export{n as default};
