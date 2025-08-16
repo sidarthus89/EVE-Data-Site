3fb154874e39b4974c3384752dc013d08a42e5ee
Sidarthus89 <sidarthus89@gmail.com>
2025-08-16 01:34:35 -0400

    Updates

---
+++ assets/Minerals-DjU5KMZp.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CB4xd9rp.js";function n(){return e.jsxs("section",{className:"minerals",children:[e.jsx("h1",{children:"Minerals"}),e.jsx("p",{children:"In development"})]})}export{n as default};
