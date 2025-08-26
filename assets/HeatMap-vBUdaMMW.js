b3977a340c2911e1bb2c8ee708bda116929b2f3b
Sidarthus89 <sidarthus89@gmail.com>
2025-08-25 20:39:05 -0400

    Updates

---
+++ assets/HeatMap-vBUdaMMW.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B5OL3X9J.js";import"./react-vendor-BIXhp891.js";function n(){return e.jsxs("section",{className:"heat-map",children:[e.jsx("h1",{children:"Heat Map"}),e.jsx("p",{children:"In development"})]})}export{n as default};
--- assets/Ores-BidxQjkA.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Bf3-lqD2.js";import"./react-vendor-BIXhp891.js";function r(){return e.jsxs("section",{className:"ores",children:[e.jsx("h1",{children:"Ores"}),e.jsx("p",{children:"In development"})]})}export{r as default};
