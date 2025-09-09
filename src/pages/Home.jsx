a54e49944a458d11218d6cd8d0bfb917155fcf6a
Sidarthus89 <sidarthus89@gmail.com>
2025-09-09 19:53:40 -0400

    Update home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,40 @@ import { Link } from 'react-router-dom';
 export default function Home() {
     return (
         <section className="home">
-            <h1>Available Tools</h1>
+            <h1>Full Market</h1>
+
+
+            <h1>Full Market</h1>
+            <p>
+                This featue is just like the in game market feature, but with some perks. Like:
+            </p>
             <ul>
-                <li><Link to="/market">Full Market(Quickbar function still in progess)</Link></li>
-                {/* <li><Link to="/appraisal">Appraisal</Link></li> */}
+                <li>The ability to serach all regions at once.</li>
+                <li>Markt distribution charts (supply and demand).</li>
+                <li>Average prciing for items.</li>
+                <li>Outlifer filtering via Interquartile Ranges.</li>
+                <li>You can click on a station locaiton and it will copy it to your clipload.</li>
+                <li>Savable/Shareable Link to items.</li>
+                <li>Add to quickbar from the item marekt view.</li>
+                <li>PLEX ticker for universe wide market high, low and average.</li>
+                <li>Sort and filter by Security Level, Region, NPC/Player locations.</li>
+            </ul>
+
+            <h1>Region to Region Trading</h1>
+            <p>
+                This has been my attempt at recreating EveTrade's setup. It allows you to set critera fr a trade route between two regions and helps you figure out the best trade combos to make to make a profit.
+            </p>
+            <ul>
+                <li>Orders update every 10 minutes</li>
+                <li>You can click to copy the item name, the From location and the To location.</li>
+                <li>Trade Route is currently not working</li>
             </ul>
 
             <h1>Known Issues (As of: 8/29/2025)</h1>
             <ul>
-                <li>Link to item on market not working</li>
+                <li>Shareable ink to item on market not working</li>
                 <li>Appraisal page is blank</li>
-                <li>Trade Route is currently not working</li>
-            </ul>
-
-            <h1>Planned Features</h1>
-            <ul>
-                <li>Ores & Mineral Sourcin and Profits</li>
+                <li>Player structure names might be incorrect or missing in areas.</li>
             </ul>
 
         </section>
